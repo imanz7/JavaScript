27e0cb10aaef8581880b40f8b993a5f5656f4f23
Iman Zahurin <73816908+imanz7@users.noreply.github.com>
2022-01-25 16:36:58 +0800

    Add files via upload

---
+++ Stopwatch/index.js
@@ -0,0 +1,33 @@
+function Stopwatch() {
+    let duration = 0;
+    let pause = true;
+
+    this.reset = function() {
+        duration = 0;
+    };
+
+    this.start = function() {
+        if(!pause)
+            throw new Error ('Stopwatch has already started.');
+        
+        pause = false;
+        duration -= Date.now();
+    };
+
+    this.stop = function() {
+        if(pause)
+            throw new Error ('Stopwatch is not started.');
+
+        pause = true;
+        duration += Date.now();
+    };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            return (duration/1000);
+        }
+    });
+}
+
+// const sw = new Stopwatch();
+//sw.duration = 2;
